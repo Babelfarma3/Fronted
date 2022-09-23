9079aae97aae8db2434b307a7399d3dfb5c7ef4a
Jeremy Jared Alcala Ocxas <u202016160@upc.edu.pe>
2022-09-22 22:27:28 -0500

    Frame4 V1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
+import { Frame4Component } from './components/frame4/frame4.component';
 import { Frame1Component } from './components/frame1/frame1.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { AngularMaterialModule } from './components/shared/angular-material/angular-material.module';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+
 const routes: Routes = [
   {path: '', component: Frame1Component},
+  {path: 'registro', component: Frame4Component}
 ];
 
 @NgModule({
