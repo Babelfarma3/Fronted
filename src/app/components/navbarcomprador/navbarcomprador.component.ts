f43ab5b68332a82447962cdfa133cb9e199b8837
Jeremy Jared Alcala Ocxas <u202016160@upc.edu.pe>
2022-09-23 19:31:41 -0500

    navbar comprador

--- src/app/components/navbarcomprador/navbarcomprador.component.ts
+++ src/app/components/navbarcomprador/navbarcomprador.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import {FormControl} from '@angular/forms';
 @Component({
   selector: 'app-navbarcomprador',
   templateUrl: './navbarcomprador.component.html',
   styleUrls: ['./navbarcomprador.component.css']
 })
+
 export class NavbarcompradorComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
