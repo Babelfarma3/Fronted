81f80823a04401b947854358c0d3dd5838fc90d0
AngelB) <gabrielo.09.angel@gmail.com>
2022-11-17 01:25:07 -0500

    carrito

--- src/app/components/frame15/frame15.component.ts
+++ src/app/components/frame15/frame15.component.ts
@@ -1,4 +1,7 @@
-import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ProductService } from './../../services/product.service';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 import { DistritoService } from './../../services/distrito.service';
 import { Distrito } from './../../models/distrito';
 import { Product } from './../../models/product';
@@ -16,26 +19,59 @@ export class Frame15Component implements OnInit {
   productosCarrito:Product[]=[];
   idDistrito!: number;
   distritos!: Distrito[];
+  subtotalb:any;
+  cantidades: any[]=[];
+  myForm!: FormGroup;
+  
 
   constructor(
     private carritoService: CarritoDeComprasService,
     private distritoService: DistritoService,
+    private productService: ProductService,
     private fb: FormBuilder,
+    private snackBar: MatSnackBar,
+    private router: Router,
   ) {
     this.getDistritos();
+    this.reactiveForm();
+    this.productosCarrito=this.carritoService.getproductosCarrito();
+    this.recuperarValores()
     
    }
 
 
   ngOnInit(): void {
-   this.productosCarrito=this.carritoService.getproductosCarrito();
+   
+
   }
   checked = false;
   disabled = false;
 
+  recuperarValores(){
+    for(let i=0;i<this.productosCarrito.length;i++){
+      this.cantidades[i]=this.myForm.get('cantidad')!.value;
+    }
+  }
+
+ 
+  getSemisuma(indice:any){ //precio e índice el producto
+
+    let cantidad= this.myForm.get('cantidad')!.value; 
+
+    this.cantidades[indice]=cantidad;
+
+  }
+
+  reactiveForm(){
+    this.myForm = this.fb.group({
+      cantidad: [1,[Validators.min(0)]],
+  })
+  }
+
 
   vaciarCarrito(){
-    this.productosCarrito= []
+    this.cantidades=[];
+    this.productosCarrito= [];
   }
 
   getDistritos(): void{
@@ -46,14 +82,44 @@ export class Frame15Component implements OnInit {
 
   subtotal():number{
 
-    let x= 0;
+    let x=0;
+    for(let i = 0 ; i < this.cantidades.length ; i++){
+      
+      x+=this.cantidades[i]*this.productosCarrito[i].precio;
 
-    this.productosCarrito.forEach(element => {
-      x+=element.precio;
-    });
+    }
+      return x;
+  }
 
-    return x;
+  eliminarProducto(indice:any){
+    this.cantidades.splice(indice,1);
+    this.productosCarrito.splice(indice,1);
 
   }
 
+  actualizarStock(){
+    
+    for(let i = 0 ; i < this.productosCarrito.length ; i++){
+   
+      const product: Product={
+        id: 0,
+        nombre: this.productosCarrito[i].nombre,
+        precio: this.productosCarrito[i].precio,
+        stock: this.productosCarrito[i].stock-this.cantidades[i],
+        descripcion: this.productosCarrito[i].descripcion,
+        categoria: this.productosCarrito[i].categoria,
+        picture: this.productosCarrito[i].picture
+      }
+      this.productService.updateProduct(this.productosCarrito[i].id, product).subscribe({
+        next: (data)=>{
+          this.snackBar.open('Productos comprados con éxito','',{duration: 3000});
+          this.router.navigate(['/Busqueda']);
+        }, 
+        error:(err)=>{
+          console.log(err);
+        }
+      });
+    }
+  
+  }
 }
